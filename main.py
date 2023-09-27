403acfc2ce3af2130e48a81e46cfb2322c7ae6c6
Clive Cullen <evilc01@hotmail.com>
2023-09-27 14:11:04 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# sample data can be replace by a database
+books = [
+    {"id": 1, "title": "Book 1", "author": "Author 1"},
+    {"id": 2, "title": "Book 1", "author": "Author 2"},
+    {"id": 3, "title": "Book 1", "author": "Author 3"},
+]
+
+
+# get all books
+@app.route('/books', methods=['GET'])
+def get_books():
+    return books
+
+# get a specific book by id
+@app.route('/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    for book in books:
+        if book['id']==book_id:
+            return book
+    return {'error':'Book not found'}
+
+#create a book
+@app.route('/books', methods=['POST'])
+def create_book():
+    new_book = {'id':len(books)+1, 'title':request.json['title'], 'author':request.json['author']}
+    books.append(new_book)
+    return new_book
+
+#update a book
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    for book in books:
+        if book['id'] == book_id:
+            book['title'] = request.json['title']
+            book['author'] = request.json['author']
+            return book
+    return {'error':'book not found'}
+
+
+#delete a book
+@app.route('/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            return {'data':'Book has been deleted'}
+    return {'error':'book not found'}
+
+# run the flask app
+if __name__ == '__main__':
+    app.run(debug=True)
